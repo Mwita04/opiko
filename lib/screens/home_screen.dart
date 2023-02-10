5563da389edaf7e4181fe50a2b729b576de6f8ce
Mwita04 <emcee779@gmail.com>
2023-02-10 20:31:05 +0300

    adding maps

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -95,7 +95,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (context) => SearchScreen(),
+                    builder: (context) => const SearchScreen(),
                     fullscreenDialog: true,
                   ),
                 );
--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: unused_local_variable
+
 import 'package:flutter/material.dart';
+import 'package:google_place/google_place.dart';
 
 class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
@@ -8,12 +11,60 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
+  final _startSearchFieldController = TextEditingController();
+  final _endSearchFieldController = TextEditingController();
+
+  // GooglePlace googlePlace;
+
+  @override
+  void initState() {
+    //TO DO: Implement initState
+    super.initState();
+    String apiKey = '';
+    //googlePlace = GooglePlace(apiKEY);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Container(
-        color: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        leading: const BackButton(color: Colors.black),
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _startSearchFieldController,
+              autofocus: false,
+              style: const TextStyle(fontSize: 24),
+              decoration: InputDecoration(
+                hintText: 'Starting Point',
+                hintStyle:
+                    const TextStyle(fontWeight: FontWeight.w500, fontSize: 24),
+                filled: true,
+                fillColor: Colors.grey[200],
+                border: InputBorder.none,
+              ),
+            ),
+            const SizedBox(height: 10),
+            TextField(
+              controller: _endSearchFieldController,
+              autofocus: false,
+              style: const TextStyle(fontSize: 24),
+              decoration: InputDecoration(
+                hintText: 'End Point',
+                hintStyle:
+                    const TextStyle(fontWeight: FontWeight.w500, fontSize: 24),
+                filled: true,
+                fillColor: Colors.grey[200],
+                border: InputBorder.none,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
